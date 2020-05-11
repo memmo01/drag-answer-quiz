85f598adf8f163586b431844298fbc532e7fb40b
michael emmons <memmons18@gmail.com>
2020-05-11 19:36:59 -0400

    added ability for questions and answers to be displayed dynamically on the page by grabbing information from array.

--- js/script.js
+++ js/script.js
@@ -5,11 +5,25 @@ let answerObj = [
     answerOptions: [1000, 50, 20],
     correctAnswer: 1000,
   },
+  {
+    question: "What is you favorite emotion?",
+    answerOptions: ["happy", "joy", "sad"],
+    correctAnswer: "happy",
+  },
 ];
 // keep track of users correct and incorrect answers
 let score = [];
-let submit = document.getElementById("submit");
+
+//keep track of question user is on
+let count = 0;
+
+//grab dom elements
 let answerSection = document.getElementById("drag-answer-section");
+let dragTextId = document.getElementById("dragText");
+let questionSection = document.querySelector(".question-section");
+let answersContain = document.querySelector(".answer-container");
+let submit = document.getElementById("submit");
+
 // drag content functions ---------
 function dragstart_handler(e) {
   e.currentTarget.style.backgroundColor = "rgba(255,255,255,.5)";
@@ -42,15 +56,26 @@ function drop_handler(e) {
   }
 }
 
-function answerBoxTextCheck(dragText, dragTextBox) {
-  let dragTextId = document.getElementById("dragText");
-  if (dragText) {
-    dragTextBox.textContent = "";
-  } else {
-    dragTextId.textContent = "Drag Answer Here";
+function start() {
+  let x = document.querySelector(".answer-container");
+
+  let pTag = document.createElement("p");
+  pTag.textContent = answerObj[count].question;
+
+  questionSection.appendChild(pTag);
+  for (let i = 0; i < answerObj[count].answerOptions.length; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "answer-box");
+    div.setAttribute("id", "a" + i);
+    div.setAttribute("ondragstart", "dragstart_handler(event)");
+    div.setAttribute("draggable", "true");
+    div.setAttribute("data-num", answerObj[count].answerOptions[i]);
+    div.setAttribute("data-answerindex", i);
+    div.textContent = answerObj[count].answerOptions[i];
+    x.appendChild(div);
   }
 }
-
+start();
 // run functions to make sure the question has been submitted correctly and then check answer to see if it is correct
 function runSubmissionCheck() {
   if (checkForInput()) {
@@ -76,8 +101,10 @@ function checkForInput() {
 function checkAnswer() {
   let draggedAnswerData = answerSection.children[1].dataset;
   let index = parseInt(draggedAnswerData.answerindex);
+  let answerArea = document.getElementById("drag-answer-section");
 
-  let result = draggedAnswerData.num == answerObj[index].correctAnswer;
+  // compare the chosen option with the correct answer
+  let result = draggedAnswerData.num == answerObj[count].correctAnswer;
 
   let record = {
     index: index,
@@ -86,6 +113,34 @@ function checkAnswer() {
 
   //push recorded answer to the score array
   score.push(record);
+  count++;
+
+  // get html ready for next questions by clearing dom sections
+  clearSections(answerArea);
+
+  // check if there are any questions left. If not then end quiz
+  answerBoxTextCheck();
+  if (count < answerObj.length) {
+    start();
+  } else {
+    alert("finished!");
+    answerArea.innerHTML = "";
+  }
+}
+
+function clearSections(answerArea) {
+  questionSection.innerHTML = "";
+  answersContain.innerHTML = "";
+  answerArea.removeChild(answerArea.lastChild);
+}
+
+//checks if answer is in answer box. If it is not they it will display text stating 'Drag Answer Here'
+function answerBoxTextCheck(dragText, dragTextBox) {
+  if (dragText) {
+    dragTextBox.textContent = "";
+  } else {
+    dragTextId.textContent = "Drag Answer Here";
+  }
 }
 
 submit.addEventListener("click", function (e) {
